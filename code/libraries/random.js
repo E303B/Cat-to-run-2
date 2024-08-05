5a1ef1fcba4590f95cb73ca92d81b10dd38e0f99
Error303bul <I-grishin-i@yandex.ru>
2024-08-05 13:27:02 +0300

    Added saws

---
+++ code/libraries/random.js
@@ -0,0 +1,6 @@
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function randNum(min, max) {
+    return Math.random() * (max - min + 1) + min;
+}
---
+++ code/libraries/vector.js
@@ -0,0 +1,3 @@
+const distanse = (x1, y1, x2, y2) => {
+    return Math.pow((((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1))), 0.5);
+}
